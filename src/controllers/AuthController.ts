abf6b16b4cd94f04adc92829c05bbc3a94ebeb4d
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-12-03 17:42:22 +0600

    refector tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,8 +4,6 @@ import { RegisterUserRequest } from '../types'
 import { UserService } from '../services/UserService'
 import { Logger } from 'winston'
 import { validationResult } from 'express-validator'
-import { RefreshToken } from '../entity/RefreshToken'
-import { AppDataSource } from '../config/data-source'
 import { TokenService } from '../services/TokenService'
 
 export class AuthController {
@@ -51,13 +49,15 @@ export class AuthController {
             const accessToken = this.tokenService.generateAccessToken(payload)
 
             // persist the refresh token
-            const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
-            const refreshTokenRepository =
-                AppDataSource.getRepository(RefreshToken)
-            const newRfreshToken = await refreshTokenRepository.save({
-                user: user,
-                expiresAt: new Date(Date.now() + MS_IN_YEAR),
-            })
+            const newRfreshToken =
+                await this.tokenService.persistRefreshToken(user)
+            // const MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
+            // const refreshTokenRepository =
+            //     AppDataSource.getRepository(RefreshToken)
+            // const newRfreshToken = await refreshTokenRepository.save({
+            //     user: user,
+            //     expiresAt: new Date(Date.now() + MS_IN_YEAR),
+            // })
 
             // const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
             //     algorithm: 'HS256',
