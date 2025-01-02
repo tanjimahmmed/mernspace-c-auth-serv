e458d8af654098bc99b0f2ee524cbdd5e501e80a
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-01-02 11:10:14 +0600

    add test to exculde password field

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -132,7 +132,7 @@ describe('POST /auth/register', () => {
 
             // Assert
             const userRepository = connection.getRepository(User)
-            const users = await userRepository.find({ select: ['password'] })
+            const users = await userRepository.find()
             expect(users[0].password).not.toBe(userData.password)
             expect(users[0].password).toHaveLength(60)
             expect(users[0].password).toMatch(/^\$2[a|b]\$\d+\$/)
--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -70,5 +70,35 @@ describe('GET /auth/self', () => {
             // Check if user id matches with registered user
             expect((response.body as Record<string, string>).id).toBe(data.id)
         })
+
+        it('should not return the password field', async () => {
+            // Register user
+            const userData = {
+                firstName: 'Tanjim',
+                lastName: 'Emmett',
+                email: 'tanjim@gmail.com',
+                password: 'secret123',
+            }
+            const userRepository = connection.getRepository(User)
+            const data = await userRepository.save({
+                ...userData,
+                role: Roles.CUSTOMER,
+            })
+            // Generate token
+            const accessToken = jwks.token({
+                sub: String(data.id),
+                role: data.role,
+            })
+            // add to token cookie
+            const response = await request(app)
+                .get('/auth/self')
+                .set('Cookie', [`accessToken=${accessToken};`])
+                .send()
+            // Assert
+            // Check if user id matches with registered user
+            expect(response.body as Record<string, string>).not.toHaveProperty(
+                'password',
+            )
+        })
     })
 })
