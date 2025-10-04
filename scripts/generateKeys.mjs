5ed912b2e54d40ff854f9bd46f82bf452bc96833
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-10-04 11:51:28 +0600

    linting fix

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,17 +1,16 @@
 import crypto from 'crypto';
 import fs from 'fs';
 
-
-const {privateKey, publicKey} = crypto.generateKeyPairSync('rsa', {
+const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
     modulusLength: 2048,
     publicKeyEncoding: {
         type: 'pkcs1',
-        format: 'pem'
+        format: 'pem',
     },
     privateKeyEncoding: {
         type: 'pkcs1',
-        format: 'pem'
-    }
+        format: 'pem',
+    },
 });
 
 console.log('Public Key', publicKey);
@@ -19,4 +18,3 @@ console.log('Private Key', privateKey);
 
 fs.writeFileSync('certs/private.pem', privateKey);
 fs.writeFileSync('certs/public.pem', publicKey);
-
