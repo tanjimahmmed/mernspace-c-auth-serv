c0baaac484cd2dcb27fdb5b5c386f7c78b1239fb
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-10-10 18:11:58 +0600

    add tenant list

--- src/services/TenantService.ts
+++ src/services/TenantService.ts
@@ -10,4 +10,8 @@ export class TenantService {
     async update(id: number, tenantData: ITenant) {
         return await this.tenantRepository.update(id, tenantData);
     }
+
+    async getAll() {
+        return await this.tenantRepository.find();
+    }
 }
