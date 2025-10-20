c54049d4549ab1285427713736359f9715e19e9d
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-10-20 17:33:54 +0600

    change origin port

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import userRouter from './routes/user';
 const app = express();
 app.use(
     cors({
-        origin: ['http://localhost:5174'],
+        origin: ['http://localhost:5173'],
         credentials: true,
     }),
 );
