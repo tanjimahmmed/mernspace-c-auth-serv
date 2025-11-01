5d23a1d742fe06acc14b1c1560084e4362e9af3c
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-11-01 19:46:41 +0600

    add filters

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,6 +1,6 @@
 import { User } from '../entity/User';
 import { LimitedUserData, UserData, UserQueryParams } from '../../types';
-import { Repository } from 'typeorm';
+import { Brackets, Repository } from 'typeorm';
 import bcrypt from 'bcryptjs';
 import createHttpError from 'http-errors';
 import logger from '../config/logger';
@@ -92,10 +92,30 @@ export class UserService {
     }
 
     async getAll(validatedQuery: UserQueryParams) {
-        const queryBuilder = this.userRepository.createQueryBuilder();
+        const queryBuilder = this.userRepository.createQueryBuilder('user');
+
+        if (validatedQuery.q) {
+            const searchTerm = `%${validatedQuery.q}%`;
+            queryBuilder.where(
+                new Brackets((qb) => {
+                    qb.where(
+                        "CONCAT(user.firstName,' ', user.lastName) ILIKE :q",
+                        { q: searchTerm },
+                    ).orWhere('user.email ILIKE :q', { q: searchTerm });
+                }),
+            );
+        }
+
+        if (validatedQuery.role) {
+            queryBuilder.andWhere('user.role = :role', {
+                role: validatedQuery.role,
+            });
+        }
+
         const result = await queryBuilder
             .skip((validatedQuery.currentPage - 1) * validatedQuery.perPage)
             .take(validatedQuery.perPage)
+            .orderBy('user.id', 'DESC')
             .getManyAndCount();
 
         return result;
